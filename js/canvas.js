cb784dc1e71151e34338651ff38d4eaa78882d6d
Elizarov Andrei <elizarov13@yandex.ru>
2018-12-13 23:49:02 +0300

    added canvas

--- js/canvas.js
+++ js/canvas.js
@@ -1,37 +1,53 @@
-
-
-
-let canvas = document.querySelector('.canvas');
-
-let ctx = canvas.getContext('2d');
+//переменные
+let canvas = document.querySelectorAll('.canvas');
 let max = 100;
 let x = 0;
-function draw() {
+let k = (2 * Math.PI) / 100;
 
-    let k = (2 * Math.PI) / 100;
-    if (x <= max) {
+//массив из canvas
+let ctxArr = [];
+canvas.forEach(elem => {
+    ctxArr.push(elem.getContext('2d'))
+})
 
-        ctx.clearRect(0, 0, 50, 50);
-        ctx.beginPath();
-        ctx.arc(50, 50, 45, 0, x * k, false);
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = "#2aa9e0";
-        ctx.lineCap = "round";
-        ctx.stroke();
-        ctx.closePath();
-        x++;
-        timeout = setTimeout(draw, 10);
 
-        ctx.beginPath();
-        ctx.font = "25px Arial";
-        ctx.textAlign = "center";
-        ctx.fillStyle = "#2aa9e0";
-        ctx.fillText("1", 50, 55);
-        ctx.closePath();
-
-    } else {
-        clearTimeout(timeout)
+//событие при скролле
+window.onscroll = function () {
+    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    console.log(scrolled)
+    if (scrolled > 5 ) {
+        drawCircle(ctxArr[0], 1);
+    } if (scrolled > 300 ) {
+        drawCircle(ctxArr[1], 2);
+    } if (scrolled > 800 ) {
+        drawCircle(ctxArr[2], 3);
+    } if (scrolled > 1300) {
+        drawCircle(ctxArr[3], 4);
     }
 }
 
-draw()
+//показать круг
+function drawCircle(ctx, num) {
+    ctx.clearRect(0, 0, 100, 100);
+    ctx.beginPath();
+    ctx.arc(50, 50, 45, 0, x * k, false);
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#2aa9e0";
+    ctx.lineCap = "round";
+    ctx.stroke();
+    ctx.closePath();
+    x++;
+    if (x <= max) setTimeout(drawCircle(ctx), 10);
+
+    draw(ctx, num)
+}
+
+//показать число
+function draw(ctx, num) {
+    ctx.beginPath();
+    ctx.font = "25px Arial";
+    ctx.textAlign = "center";
+    ctx.fillStyle = "#2aa9e0";
+    ctx.fillText(num, 50, 59);
+    ctx.closePath();
+}
