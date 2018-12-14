a8ae6cb59cf811b41679c2921585053ff45eb86e
Elizarov Andrei <elizarov13@yandex.ru>
2018-12-14 18:07:11 +0300

    canvas finished

--- js/canvas.js
+++ js/canvas.js
@@ -1,46 +1,42 @@
 //переменные
 let canvas = document.querySelectorAll('.canvas');
 let max = 100;
-let x = 0;
+let firns = 0;
 let k = (2 * Math.PI) / 100;
 
-//массив из canvas
+// массив из canvas
 let ctxArr = [];
 canvas.forEach(elem => {
     ctxArr.push(elem.getContext('2d'))
 })
 
-
 //событие при скролле
-window.onscroll = function () {
-    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    console.log(scrolled)
-    if (scrolled > 5 ) {
-        drawCircle(ctxArr[0], 1);
-    } if (scrolled > 300 ) {
-        drawCircle(ctxArr[1], 2);
-    } if (scrolled > 800 ) {
-        drawCircle(ctxArr[2], 3);
-    } if (scrolled > 1300) {
-        drawCircle(ctxArr[3], 4);
+window.addEventListener('scroll', () => {
+    const scrolled = window.pageYOffset;
+
+    //считываем только большее значение скролла (скролл работает только вниз)
+    //при скролле вверх данные не считываются
+    if (firns < scrolled) {
+        firns += scrolled
+        firns = scrolled
+        console.log(firns)
     }
-}
 
-//показать круг
-function drawCircle(ctx, num) {
-    ctx.clearRect(0, 0, 100, 100);
-    ctx.beginPath();
-    ctx.arc(50, 50, 45, 0, x * k, false);
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "#2aa9e0";
-    ctx.lineCap = "round";
-    ctx.stroke();
-    ctx.closePath();
-    x++;
-    if (x <= max) setTimeout(drawCircle(ctx), 10);
+    if (firns >= getCoords(canvas[0])-300 && firns <= getCoords(canvas[0])) { drawCircle(5, ctxArr[0], 1) }
+    if (firns >= getCoords(canvas[1])-500 && firns <= getCoords(canvas[1])) { drawCircle(5, ctxArr[1], 2) }
+    if (firns >= getCoords(canvas[2])-500 && firns <= getCoords(canvas[2])) { drawCircle(5, ctxArr[2], 3) }
+    if (firns >= getCoords(canvas[3])-500 && firns <= getCoords(canvas[3])) { drawCircle(5, ctxArr[3], 4) }
 
-    draw(ctx, num)
+})
+
+
+//координата объекта
+function getCoords(box1) {
+    var box = box1.getBoundingClientRect();
+    return box.top + pageYOffset;
 }
+console.log(getCoords(canvas[3]))
+
 
 //показать число
 function draw(ctx, num) {
@@ -48,6 +44,27 @@ function draw(ctx, num) {
     ctx.font = "25px Arial";
     ctx.textAlign = "center";
     ctx.fillStyle = "#2aa9e0";
-    ctx.fillText(num, 50, 59);
+    ctx.fillText(num, 50, 55);
     ctx.closePath();
+}
+
+//рисовать круг
+function drawCircle(time, ctx, num) {
+    let x = 1;
+    let timeID = setInterval(() => {
+        if (x <= max) {
+            //ctx.clearRect(0, 0, 100, 100);
+            ctx.beginPath();
+            ctx.arc(50, 50, 45, (x - 1) * k, x * k, false);
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = "#2aa9e0";
+            ctx.lineCap = "round";
+            ctx.stroke();
+            ctx.closePath();
+            x++;
+        } else {
+            clearInterval(timeID)
+        }
+    }, time);
+    draw(ctx, num)
 }
